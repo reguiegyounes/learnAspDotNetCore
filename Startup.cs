a6e386e8f9f1aac196d51f66b9f9ca2fe74a547e
younes <reguiegyounes@gmail.com>
2021-07-27 15:50:42 +0100

    Environment variable

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +9,12 @@ namespace WebApplication3
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -29,7 +36,8 @@ namespace WebApplication3
                 endpoints.MapGet("/", async context =>
                 {
                     var myProccess = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-                    await context.Response.WriteAsync(myProccess);
+                    var envVar=configuration["key1"];
+                    await context.Response.WriteAsync(envVar);
                 });
             });
         }
