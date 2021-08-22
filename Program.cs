8b62e8d9067ac84d6e3d82e07099b5a0af3756a5
younes <reguiegyounes@gmail.com>
2021-08-22 12:33:20 +0100

    logging to File (Nlog)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using NLog.Web;
 
 namespace learnAspDotNetCore
 {
@@ -15,6 +16,6 @@ namespace learnAspDotNetCore
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).UseNLog();
     }
 }
