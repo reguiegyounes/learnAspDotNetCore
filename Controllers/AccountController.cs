5ff12ebb83be50faaeef71897d83c4b33f38036c
younes <reguiegyounes@gmail.com>
2021-08-23 11:27:23 +0100

    use userManager and signInManager

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,22 @@
 ﻿using learnAspDotNetCore.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace learnAspDotNetCore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
 
         [HttpGet]
         public IActionResult Register()
@@ -13,13 +25,35 @@ namespace learnAspDotNetCore.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(AccountRegisterViewModel model)
+        public async Task<IActionResult> Register(AccountRegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                return View();
+                string username= GenerateUsername(model.FirstName, model.LastName);
+                IdentityUser user = new IdentityUser()
+                {
+                    UserName = username,
+                    Email = model.Email
+                };
+
+                var result =await userManager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index","Employee");
+                }
+                
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
             }
             return View(model);
+        }
+
+        private string GenerateUsername(string firstName ,string lastName)
+        {
+            return firstName.Trim() + "_" + lastName.Trim();
         }
     }
 }
