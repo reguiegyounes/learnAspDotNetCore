52a92c296ffd828b30b362a165d003ebb4bf1ef3
younes <reguiegyounes@gmail.com>
2021-08-25 18:33:27 +0100

    creating roles

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,47 @@
+﻿using learnAspDotNetCore.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learnAspDotNetCore.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(AdministrationCreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+                IdentityResult result=await roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(actionName: "Index", controllerName: "Employee");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
